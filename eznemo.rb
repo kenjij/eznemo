524ce14bdc218d847e238fa0a05398e5caea5c24
cyan.and.blue <cyan.and.blue@387b9284-5b1a-0410-a079-4f3c2114d354>
2006-09-18 15:31:14 +0000

    Added new HTML reporting feature. Some bug fix. (INCOMPLETE 0.8beta 20060917)

--- eznemo.rb
+++ eznemo.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/ruby
 #
 # eznemo.rb - A simple host monitoring with TCP ping.
-# ver.0.7beta (2006-08-21)
+# ver.0.8beta (2006-09-17)
 # (c) 2006 CYANandBLUE
 #
 # License:
@@ -28,20 +28,25 @@ require 'net/smtp'
 require 'ping'
 require 'thread'
 
+
 ##
 ## System parameters
 ##
-S_SYSTEMNAME = 'EzNemo ver.0.7beta - (c) 2006 CYANandBLUE'
+S_SYSTEMNAME = 'EzNemo ver.0.8beta - (c) 2006 CYANandBLUE'
 I_PINGTIMEOUT_MAX = 10
 I_PINGINTERVAL_MIN = 60
 I_PINGRETRY_MAX = 10
+S_HTMLBASEPATH = './status.html'
+
+S_LOGDATEFORMAT = '%Y-%m-%dT%H:%M:%S%Z'
+S_REPORTDATEFORMAT = '%c'
 
 
 ##
 ## Default setting (variables will be reset by configuration)
 ##
-$i_pingtcpport = 7
 $i_pingtimeout = 1
+$i_pingport = 7
 $i_pinginterval = 300
 $i_pingretry = 2
 $s_smtpserver = '127.0.0.1'
@@ -50,9 +55,14 @@ $s_smtpdomain = ''
 $s_emailfrom = ''
 $a_emailalarm = []
 $s_alarmsubject = 'EzNemo ALARM'
+$s_reportpath = './eznemo.html'
 
+$h_initconfig = {}
+$s_html_initconf = ''
 # $q_msg = {'time' => , 'ipaddr' => , 'status' => , 'type' => , }
 $q_msg = Queue.new
+# $h_host = {ip_addr => comment} (when reading conf)
+# $h_host = {ip_addr => #Host} (at host init)
 $h_host = {}
 
 
@@ -77,40 +87,79 @@ end
 
 # Message handler class
 #  Handles internal message outputs depending on the type.
-#  Message.event : Any action can be an event if the hosts status does not change. A line will output to the STDOUT.
-#  Message.error : Whenever something unexpected happens. A line will output to the STDERR.
-#  Message.alarm : Whenever the hosts status changes. In addition to output to the STDOUT, an email can be sent out.
-#  Message.abort : This aborts the program. Likely to be an initial error.
+#  Message.event  : Any action can be an event if the hosts status does not change. A line will output to the STDOUT.
+#  Message.stderr : System message. A line will output to the STDERR.
+#  Message.error  : Whenever something unexpected happens. A line will output to the STDERR.
+#  Message.alarm  : Whenever the hosts status changes. In addition to output to the STDOUT, an email can be sent out.
+#  Message.abort  : This aborts the program. Likely to be an initial error.
 class Message
   def initialize
   end
   def event(hash)
     if $h_host.key?(hash['ipaddr'])
-      str = "%s %s %s(%s)" % [hash['time'].strftime("%Y-%m-%dT%H:%M:%S%Z"), hash['status'], hash['ipaddr'], $h_host[hash['ipaddr']].comment]
+      str = "%s %s %s(%s)" % [hash['time'].strftime(S_LOGDATEFORMAT), hash['status'], hash['ipaddr'], $h_host[hash['ipaddr']].comment]
     else
-      str = "%s %s" % [hash['time'].strftime("%Y-%m-%dT%H:%M:%S%Z"), hash['status']]
+      str = "%s %s" % [hash['time'].strftime(S_LOGDATEFORMAT), hash['status']]
     end
     $stdout << str << "\n"
+  end
+  def stderr(str)
+    $stderr << str << "\n"
   end
   def error(hash)
   end
   def alarm(hash)
     if $h_host.key?(hash['ipaddr'])
-      str = "%s %s %s(%s)" % [hash['time'].strftime("%Y-%m-%dT%H:%M:%S%Z"), hash['status'], hash['ipaddr'], $h_host[hash['ipaddr']].comment]
+      str = "%s %s %s(%s)" % [hash['time'].strftime(S_LOGDATEFORMAT), hash['status'], hash['ipaddr'], $h_host[hash['ipaddr']].comment]
     else
-      str = "%s %s" % [hash['time'].strftime("%Y-%m-%dT%H:%M:%S%Z"), hash['status']]
+      str = "%s %s" % [hash['time'].strftime(S_LOGDATEFORMAT), hash['status']]
     end
     $stdout << str << "\n"
     email(str)
   end
-  def abort(hash)
-    if $h_host.key?(hash['ipaddr'])
-      str = "%f %s %s(%s)" % [hash['time'].to_f, hash['status'], hash['ipaddr'], $h_host[hash['ipaddr']].comment]
-    else
-      str = "%f %s" % [hash['time'].to_f, hash['status']]
-    end
-    $stderr << "\n" << str << "\n"
+  def abort(str)
+    self.stderr(str)
     exit(1)
+  end
+end
+
+# HTML report class
+class HTMLReport
+  @@template = open('status.html', 'r') {|fp| fp.read}
+  def initialize(outputpath)
+    @outputpath = outputpath
+    @h_htmlhost = {}
+    @htmlstat = ''
+    @htmlinitconf = ''
+  end
+  def update_host(ip)
+    host = $h_host[ip]
+    @h_htmlhost[ip] = "<tr><td>#{ip}</td><td>#{host.comment}</td><td class=\"#{host.status.downcase}\">#{host.status}</td></tr>\n"
+  end
+  def update_hosts
+    @h_htmlhost = {}
+    $h_host.each do |ip, host|
+      @h_htmlhost[ip] = "<tr><td>#{ip}</td><td>#{host.comment}</td><td class=\"#{host.status.downcase}\">#{host.status}</td></tr>\n"
+    end
+  end
+  def update_stat
+    @htmlstat = ''
+  end
+  def update_initconf
+    @htmlinitconf = ''
+    $h_initconfig.each do |key, val|
+      @htmlinitconf << "<tr><td>#{key}</td><td>#{val}</td></tr>\n"
+    end
+  end
+  def run
+    htmloutput = @@template.sub('<!-- ##SYSTEM_NAME## -->', S_SYSTEMNAME)
+    htmloutput = htmloutput.sub('<!-- ##UPDATE## -->', Time.now.strftime(S_REPORTDATEFORMAT))
+    htmloutput = htmloutput.sub('<!-- ##HOST## -->', @h_htmlhost.values.join)
+    htmloutput = htmloutput.sub('<!-- ##STAT## -->', @htmlstat)
+    htmloutput = htmloutput.sub('<!-- ##INIT_CONFIG## -->', @htmlinitconf)
+    fp_report = open(@outputpath, 'w')
+    fp_report.print(htmloutput)
+    fp_report.close
   end
 end
 
@@ -134,7 +183,7 @@ class Host
   end
   def ping
     @timeping = Time.now
-    pingstatus = Ping.pingecho(@ipaddr, $i_pingtimeout, $i_pingtcpport)
+    pingstatus = Ping.pingecho(@ipaddr, $i_pingtimeout, $i_pingport)
     if pingstatus
       case @status
       when 'INIT'
@@ -183,86 +232,96 @@ end
 $msg = Message.new
 
 ## Read configuration
-$stderr << "CONFIG: Reading configuration... "
+$msg.stderr("CONFIG: Reading configuration...")
 begin
   i = 0
   ARGF.each do |line|
     i += 1
-    if line =~ /^(#.*|)$/
+    if /^(#.*|)$/ =~ line
       next
-    elsif line =~ /^([\w]+) *= *(.+)$/
+    elsif /^([\w]+) *= *(.+)$/ =~ line
       key, val = [$1, $2]
+      $h_initconfig[key]=val
       case key
       when 'PING_TIMEOUT'
-        if val =~ /^\d+$/
+        if /^\d+$/ =~ val
           $i_pingtimeout = val.to_i
           if I_PINGTIMEOUT_MAX < $i_pingtimeout
-            raise 'value out of range'
+            raise "value out of range : #{key}=#{val}"
           end
         else
-          raise 'wrong value'
+          raise "wrong value : #{key}=#{val}"
+        end
+      when 'PING_PORT'
+        if /^\d+$/ =~ val
+          $i_pingport = val.to_i
+          if 0 > $i_pingport or 65535 < $i_pingport
+            raise "value out of range : #{key}=#{val}"
+          end
+        else
+          raise "wrong value : #{key}=#{val}"
         end
       when 'PING_INTERVAL'
-        if val =~ /^\d+$/
+        if /^\d+$/ =~ val
           $i_pinginterval = val.to_i
           if I_PINGINTERVAL_MIN > $i_pinginterval
-            raise 'value out of range'
+            raise "value out of range : #{key}=#{val}"
           end
         else
-          raise 'wrong value'
+          raise "wrong value : #{key}=#{val}"
         end
       when 'PING_RETRY'
-        if val =~ /^\d+$/
+        if /^\d+$/ =~ val
           $i_pingretry = val.to_i
           if I_PINGRETRY_MAX < $i_pingretry
-            raise 'value out of range'
+            raise "value out of range : #{key}=#{val}"
           end
         else
-          raise 'wrong value'
+          raise "wrong value : #{key}=#{val}"
         end
       when 'SMTP_SERVER'
-        if val =~ /^[A-Za-z0-9\-\.]+$/
+        if /^[A-Za-z0-9\-\.]+$/ =~ val
           $s_smtpserver = val
         else
-          raise 'wrong value'
+          raise "wrong value : #{key}=#{val}"
         end
       when 'SMTP_PORT'
-        if val =~ /^\d+$/
+        if /^\d+$/ =~ val
           $i_smtpport = val.to_i
           if 0 > $i_smtpport or 65536 < $i_smtpport
-            raise 'value out of range'
+            raise "value out of range : #{key}=#{val}"
           end
         else
-          raise 'wrong value'
+          raise "wrong value : #{key}=#{val}"
         end
       when 'SMTP_DOMAIN'
-        if val =~ /^[A-Za-z0-9\-\.]+$/
+        if /^[A-Za-z0-9\-\.]+$/ =~ val
           $s_smtpdomain = val
         else
-          raise 'wrong value'
+          raise "wrong value : #{key}=#{val}"
         end
       when 'EMAIL_FROM'
-        if val =~ /^[^@\s]+@[A-Za-z0-9\-\.]+$/
+        if /^[^@\s]+@[A-Za-z0-9\-\.]+$/ =~ val
           $s_emailfrom = val
         else
-          raise 'wrong value'
+          raise "wrong value : #{key}=#{val}"
         end
       when 'EMAIL_ALARM'
-        if val =~ /^[^@\s]+@[A-Za-z0-9\-\.]+$/
+        if /^[^@\s]+@[A-Za-z0-9\-\.]+$/ =~ val
           $a_emailalarm << val
         else
-          raise 'wrong value'
+          raise "wrong value : #{key}=#{val}"
         end
       when 'ALARM_SUBJECT'
-        if val =~ /^.+$/
-          $s_alarmsubject << val
+        if /^.+$/ =~ val
+          $s_alarmsubject = val
         else
-          raise 'wrong value'
+          raise "wrong value : #{key}=#{val}"
         end
       else
         raise 'unrecognized varialble'
       end
-    elsif line =~ /^([\.\d]{7,15}) *= *(.+)$/
+    elsif /^([\.\d]{7,15}) *= *(.+)$/ =~ line
       ipaddr = IPAddr.new($1)
       $h_host[ipaddr.to_s] = $2
     else
@@ -272,13 +331,18 @@ begin
 rescue
   $msg.abort("CONFIG: Configuration error at line #{i}. (#{$!})")
 end
-$stderr << "done.\n"
+$h_initconfig['EMAIL_ALARM'] = $a_emailalarm.join(',')
+$msg.stderr("done.")
+
+$report = HTMLReport.new($s_reportpath)
+$report.update_initconf
+$report.run
 
 
 ##
 ## Message handling thread
 ##
-$stderr << "MSG: Starting message handler.\n"
+$msg.stderr("MSG: Starting message handler.")
 #$q_msg << {'time' => Time.now, 'ipaddr' => localhost, 'status' => 'MSG: Up.', 'type' => 'event'}
 Thread.start {
 begin
@@ -297,7 +361,7 @@ end
 ##
 ## Start monitoring thread for each IP
 ##
-$stderr << "MONITOR: Starting #{$h_host.length} host monitoring sessions.\n"
+$msg.stderr("MONITOR: Starting #{$h_host.length} host monitoring sessions.")
 $h_host.each do |ipaddr, comment|
   $h_host[ipaddr] = Host.new(ipaddr, comment)
   Thread.start {
@@ -307,6 +371,10 @@ $h_host.each do |ipaddr, comment|
     }
   }
 end
+
+
+$report.update_hosts
+$report.run
 
 
 ##
